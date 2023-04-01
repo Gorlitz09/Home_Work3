810dfead824080e6c95ce75703b16c181235fe5c
Gorlitz <panfyorov.ivan@mail.ru>
2023-04-01 20:19:06 +0300

    W

---
+++ C#_19/Program.cs
@@ -0,0 +1,19 @@
+﻿/*Напишите программу, которая принимает 
+на вход пятизначное число и проверяет, является ли оно палиндромом
+14212 -> нет
+23432 -> да
+12821 -> да*/
+
+Console.Clear();
+Console.Write("Введите число: ");
+string num = Console.ReadLine();
+void Number (string num){
+    if (num[0] == num [4] || num[1] == num [3]){
+    Console.WriteLine($"Число: {num} является палиндромом.");
+    }
+    else Console.WriteLine($"Число: {num} не является палиндромом");
+}
+if (num!.Length == 5){
+    Number(num);
+}
+else Console.WriteLine($"Введите правильное число");
